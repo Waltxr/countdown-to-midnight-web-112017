e620ba9e7ca996f9ab10253cbbf495c399c44cca
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-14 19:14:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(integer)
     puts "#{integer} SECOND(S)!"
     integer -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(integer)
@@ -14,5 +14,5 @@ def countdown_with_sleep(integer)
     sleep 1
     integer -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
